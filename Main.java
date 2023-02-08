b22d2485196f11dc79f45e7ba3cb2ff4b3b4e283
ErenGokirmak <sukruerengokirmak@gmail.com>
2023-02-08 23:32:20 +0300

    Added function specifications
    Added the necessary function specifications for the program.

---
+++ Main.java
@@ -0,0 +1,29 @@
+/*
+ * Lab0 main file
+ */
+public class Main {
+    // Needed functionality:
+    /**
+     * int[] fillArray(int arrSize): Given an array size, create an int array that
+     * contains random integers in the range [0,100].
+     * 
+     * void displayMenu(): Display the menu options, get the user’s choice, and call
+     * the required methods to print out the result. Until the user chooses to exit,
+     * you should display the menu again.
+     * 
+     * int findMin(int[] arr): Find the minimum of an array.
+     * 
+     * int findMax(int[] arr): Find the maximum of an array.
+     * 
+     * double findAvg(int[] arr): Find the average of an array.
+     * 
+     * String diffOfArray(int[] arr): Return a string representation of an array
+     * such that newArr[i] = arr[i] - findAvg(arr)).
+     * 
+     * int findSumOfOddIndexes(int[] arr): Find the sum of odd-indexed integers of
+     * an array.
+     * 
+     * int findSumOfEvenIndexes(int[] arr): Find the sum of even-indexed integers of
+     * an array.
+     */
+}
