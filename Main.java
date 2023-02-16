0f50a0f4fc4b33c8adef6c8c0c1244e7977bfb27
Emir Ensar <esevil604@gmail.com>
2023-02-16 19:41:04 +0300

    şaaak

--- Main.java
+++ Main.java
@@ -17,10 +17,10 @@ public class Main {
      * 
      * int findMax(int[] arr): Find the maximum of an array.
      * 
-     * double findAvg(int[] arr): Find the average of an array.
+     * double findAvg(int[] arr): Find the average of an array. DONE
      * 
-     * String diffOfArray(int[] arr): Return a string representation of an array
-     * such that newArr[i] = arr[i] - findAvg(arr)).
+     * String diffOfArray(int[] arr): Return an array
+     * such that newArr[i] = arr[i] - findAvg(arr)). DONE
      * 
      * int findSumOfOddIndexes(int[] arr): Find the sum of odd-indexed integers of
      * an array.
@@ -42,15 +42,44 @@ public class Main {
         Random rand = new Random();
         for (int i = 0; i < arrSize; i++) {
             randArray[i] = (int) (rand.nextDouble() * 100 + 1);
-        }
+        }        
         return randArray;
     }
-
-    public static void main(String[] args) {
-
-        // Creating the array that will be worked on
-        int[] mainArray = fillArray(100);
-
+    
+    /**
+     * Finds the average of an array.
+     * 
+     * @author Emir Ensar Sevil
+     * @param arr the array to find its average.
+     * @return the average of the array.
+     */
+    public static int findAverage(int[] arr)
+    {
+        int av = 0;
+        for( int i = 0; i < arr.length; i++)
+        {
+            av += arr[i]; 
+        }
+        av = av / arr.length;
+        return av;  
+    }
+    
+    /**
+     * Finds the difference between the elements of an array's elements and its average. 
+     * 
+     * @author Emir Ensar Sevil
+     * @param arr the array to use the elements.
+     * @return the reference to an array that contains the difference of the elements from its average.
+     */
+    public static int[] diffFromAv(int[] arr)
+    {
+        int av = Main.findAverage(arr);
+        int[] diffOfArray = new int[arr.length];
+        for( int i = 0; i < arr.length; i++)
+        {
+            diffOfArray[i] = arr[i] - av;
+        }
+        return diffOfArray;
     }
 
 }
